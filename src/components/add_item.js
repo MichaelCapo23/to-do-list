cbceffcf5ba2a3d93d33e0f9e734714688306311
MichaelCapo23 <123pie23@gmail.com>
2018-12-19 18:05:01 -0800

    made most of the functionality, need to fix cancel, key issue

---
+++ src/components/add_item.js
@@ -0,0 +1,42 @@
+import React, { Component } from 'react';
+
+class AddItem extends Component {
+    state = {
+       title: '',
+       details: '',
+    };
+
+    handleSubmit = (event) => {
+        event.preventDefault();
+        console.log(this.state);
+        this.props.add(this.state);
+    };
+
+    render() {
+        const {title, details} = this.state;
+        return (
+            <form onSubmit={this.handleSubmit}>
+                <div className="row">
+                    <div className="input-field s8 col offset-s2">
+                        <input onChange={(event) => this.setState({title:event.target.value})} value={title} id="title" type="text" name="title" autoComplete="off"/>
+                        <label htmlFor={"title"}>Title</label>
+                    </div>
+                    <div className="input-field s8 col offset-s2">
+                        <input onChange={(event) => this.setState({details:event.target.value})}  value={details} id="details" type="text" name="details" autoComplete="off"/>
+                        <label htmlFor={"title"}>Details</label>
+                    </div>
+                </div>
+                <div className={"row"}>
+                    <div className="col s6 center">
+                        <button  type={"button"} className={"btn red waves-effect waves-light"}>Cancel</button>
+                    </div>
+                    <div className="col s6 center">
+                        <button className={"btn green waves-effect waves-light"}>Add Item</button>
+                    </div>
+                </div>
+            </form>
+        )
+    }
+}
+
+export default AddItem;
--- src/components/app.js
+++ src/components/app.js
@@ -1,12 +1,44 @@
 import 'materialize-css/dist/css/materialize.min.css';
-import React from 'react';
-import List from './list'
+import React, {Component} from 'react';
+import List from './list';
+import AddItem from './add_item';
+import 'materialize-css/dist/js/materialize';
+import dummyList from "../data/to_do_list";
 
-const App = () => (
-    <div className={"container"}>
-        <h1 className={"center"}>TO DO LIST</h1>
-        <List/>
-    </div>
-);
+class App extends Component {
+    state = {
+        list: [],
+
+    };
+
+    componentDidMount() {
+        this.getListData();
+    }
+
+    addItem = (item) => {
+        const {list} = this.state;
+        this.setState({
+            list: [item, ...list],
+        });
+    };
+
+    getListData = () => {
+        this.setState({
+            list: dummyList,
+        })
+    };
+
+    render() {
+        const {list} = this.state;
+        return (
+            <div className={"container"}>
+                <h1 className={"center"}>TO DO LIST</h1>
+                <AddItem add={this.addItem}/>
+                <List toDos={list}/>
+            </div>
+        );
+    }
+}
+
 
 export default App;
