5faf1cd1ec20c0870227902f28fecd85ccb26c35
MichaelCapo23 <123pie23@gmail.com>
2018-12-27 13:13:54 -0800

    seperated pages

--- src/components/list.js
+++ src/components/list.js
@@ -1,16 +1,19 @@
 import React from 'react';
 import ListItem from './list_item'
+import NavButton from './nav_buuton'
 
 const List = (props) => {
-
     const listElements = props.toDos.map(item => {
         return <ListItem toggle={() => props.toggle(item._id)} delete={() => props.delete(item._id)} key={item._id} title={item.title} description={item.description} complete={item.complete}/>
     });
     return (
-        <ul className={"collection"}>
-            {listElements}
-        </ul>
-
+        <div>
+            <h1 className={"center"}>To Do List</h1>
+            <NavButton to={"/add-item"} text="Add Item"/>
+            <ul className={"collection"}>
+                {listElements}
+            </ul>
+        </div>
     )
 }
 
